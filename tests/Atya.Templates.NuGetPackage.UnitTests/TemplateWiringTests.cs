9dee5718fd0cfb859ea6c29f0f1d942d1e7c370c
aasulyan <arsen.asulyan.1991@gmail.com>
2026-06-11 01:28:07 +0400

    chore: clarify template wiring checks

--- tests/Atya.Templates.NuGetPackage.UnitTests/TemplateWiringTests.cs
+++ tests/Atya.Templates.NuGetPackage.UnitTests/TemplateWiringTests.cs
@@ -5,7 +5,7 @@ public sealed class TemplateWiringTests
     [Fact]
     public void LibraryAssembly_Can_Be_Loaded()
     {
-        var assembly = typeof(Atya.Templates.NuGetPackage.__PACKAGE_MARKER_NAME__Marker).Assembly;
+        var assembly = typeof(__PACKAGE_MARKER_NAME__Marker).Assembly;
 
         assembly.Should().NotBeNull();
         assembly.GetName().Name.Should().Be("Atya.Templates.NuGetPackage");
